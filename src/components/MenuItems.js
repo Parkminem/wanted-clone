9540466b6fb7e6f69c7d5d410b200eb5fbd9a3e4
MinhyukPark <parkminem@gmail.com>
2021-07-19 19:47:10 +0900

    no message

---
+++ src/components/MenuItems.js
@@ -0,0 +1,42 @@
+export const MenuItems = [
+  {
+    title: "기업영업",
+    path: "/marketing",
+    Cname: "dropdown-link",
+  },
+  {
+    title: "외부영업",
+    path: "/consulting",
+    Cname: "dropdown-link",
+  },
+  {
+    title: "영업 관리자",
+    path: "/design",
+    Cname: "dropdown-link",
+  },
+  {
+    title: "기술영입",
+    path: "/development",
+    Cname: "dropdown-link",
+  },
+  {
+    title: "주요고객사 담당자",
+    path: "/development",
+    Cname: "dropdown-link",
+  },
+  {
+    title: "솔루션 컨설턴트",
+    path: "/development",
+    Cname: "dropdown-link",
+  },
+  {
+    title: "해외영입",
+    path: "/development",
+    Cname: "dropdown-link",
+  },
+  {
+    title: "더보기",
+    path: "/development",
+    Cname: "dropdown-link",
+  },
+];
